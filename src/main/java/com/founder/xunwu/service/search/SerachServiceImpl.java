84567b0e5b18c3ced6fbe140dc6aaa131514e473
yangming <ccutyang0569@sina.com>
2018-03-07 23:46:26 +0800

    update

--- src/main/java/com/founder/xunwu/service/search/SerachServiceImpl.java
+++ src/main/java/com/founder/xunwu/service/search/SerachServiceImpl.java
@@ -3,12 +3,18 @@ package com.founder.xunwu.service.search;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.founder.xunwu.entity.House;
+import com.founder.xunwu.entity.HouseDetail;
+import com.founder.xunwu.entity.HouseTag;
+import com.founder.xunwu.entity.SupportAddress;
 import com.founder.xunwu.repository.HouseDetailRespository;
 import com.founder.xunwu.repository.HouseRespository;
 import com.founder.xunwu.repository.HouseTagRepository;
 import com.founder.xunwu.repository.SupportAddressRepository;
 import com.founder.xunwu.service.house.IAddressService;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentType;
@@ -21,8 +27,13 @@ import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @program: xunwu
@@ -43,7 +54,7 @@ public class SerachServiceImpl implements ISerachService {
     private static final String INDEX_TOPIC = "house_build";
 
     @Autowired
-    private HouseRespository  hosueRespository;
+    private HouseRespository  houseRespository;
 
     @Autowired
     private HouseDetailRespository houseDetailRespository;
@@ -64,18 +75,136 @@ public class SerachServiceImpl implements ISerachService {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
+    @KafkaListener(topics=INDEX_TOPIC)
+    public void handleMassage(String content){
+
+        try {
+            HouseIndexMessage message = objectMapper.readValue(content, HouseIndexMessage.class);
+            switch(message.getOperation()){
+
+                case HouseIndexMessage.INDEX:
+                    this.createOrUpdate(message);
+                    break;
+                case HouseIndexMessage.REMOVE:
+                    this.removeIndex(message);
+                    break;
+                default:
+                    logger.warn("Not support message content"+content);
+                    break;
+            }
+
+        } catch (IOException e) {
+                logger.error("Cannot parse json for"+content,e);
+        }
+
+    }
+
+    private void removeIndex(HouseIndexMessage message) {
+        Long houseId = message.getHouseId();
+        DeleteByQueryRequestBuilder builder = DeleteByQueryAction
+                .INSTANCE
+                .newRequestBuilder(esClient)
+                .filter(QueryBuilders.termQuery(HouseIndexKey.HOUSE_ID, houseId))
+                .source(INDEX_NAME);
+        BulkByScrollResponse response = builder.get();
+        long deleted = response.getDeleted();
+        logger.debug("Detail total" + deleted);
+
+    }
 
 
+
+    private void createOrUpdate(HouseIndexMessage message) {
+        Long houseId = message.getHouseId();
+        House house = houseRespository.findOne(houseId);
+        if(house==null){
+              logger.error("Index house {} dose not Exist",houseId);
+              this.index(houseId,message.getRetry()+1);
+              return;
+        }
+        HouseIndexTemplate indexTemplate=new HouseIndexTemplate();
+        modelMapper.map(house,indexTemplate);
+
+        HouseDetail detail = houseDetailRespository.findByHouseId(houseId);
+        if(detail==null){
+            // TODO 异常情况
+        }
+        modelMapper.map(detail, indexTemplate);
+        SupportAddress city = supportAddressRepository.findByEnNameAndLevel(house.getCityEnName(), SupportAddress.Level.CITY.getValue());
+
+        SupportAddress region = supportAddressRepository.findByEnNameAndLevel(house.getCityEnName(), SupportAddress.Level.CITY.getValue());
+       // String address = city.getCnName() + region.getCnName() + house.getStreet() + house.getDistrict() + detail.getDetailAddress();
+
+        List<HouseTag> tags = houseTagRepository.findAllByHouseId(houseId);
+        if(tags!=null&&!tags.isEmpty()){
+            List<String> tagsString = new ArrayList<>();
+            tags.forEach(houseTag -> tagsString.add(houseTag.getName()));
+            indexTemplate.setTags(tagsString);
+
+        }
+
+        //房源索引
+
+        SearchRequestBuilder requestBuilder = this.esClient.prepareSearch(INDEX_NAME).setTypes(INDEX_TYPE).setQuery(QueryBuilders.termQuery(HouseIndexKey.HOUSE_ID, houseId));
+
+        logger.debug(requestBuilder.toString());
+
+        SearchResponse searchResponse = requestBuilder.get();
+        boolean success;
+        long totalHit = searchResponse.getHits().getTotalHits();
+        if(totalHit==0){
+            success = create(indexTemplate);
+        }else if(totalHit==1){
+            String esId = searchResponse.getHits().getAt(0).getId();
+            success=update(esId,indexTemplate);
+
+        }else{
+             success = deleteAndCreate(totalHit, indexTemplate);
+        }
+
+
+    }
 
 
     @Override
     public void index(Long houseId) {
 
+         this.index(houseId,0);
+
+    }
+
+    private void index(Long houseId,int retry){
+        if(retry>HouseIndexMessage.MAX_RETRY){
+            logger.error("Retry index times over 3 for house"+houseId+"Please check it");
+             return;
+        }
+        HouseIndexMessage message = new HouseIndexMessage(houseId, HouseIndexMessage.INDEX, retry);
+        try {
+            kafkaTemplate.send(INDEX_TOPIC, objectMapper.writeValueAsString(message));
+        } catch (JsonProcessingException e) {
+            logger.error(" Json encode error for "+message);
+        }
     }
 
     @Override
     public void remove(Long houseId) {
 
+        this.remove(houseId,0);
+
+    }
+
+    private void remove(Long houseId,int retry){
+
+        if(retry>HouseIndexMessage.MAX_RETRY){
+            logger.error("Retry remove times overs 3 for house:"+houseId+"Please check id !");
+            return ;
+        }
+        HouseIndexMessage message = new HouseIndexMessage(houseId, HouseIndexMessage.REMOVE, retry);
+        try {
+            this.kafkaTemplate.send(INDEX_TOPIC, objectMapper.writeValueAsString(message));
+        } catch (JsonProcessingException e) {
+            logger.error("Cannot encode json for " + message, e);
+        }
     }
 
     /**
@@ -130,7 +259,7 @@ public class SerachServiceImpl implements ISerachService {
      * @param houseIndexTemplate
      * @return
      */
-    public boolean deleteAndCreate(int totalHit, HouseIndexTemplate houseIndexTemplate) {
+    public boolean deleteAndCreate(Long totalHit, HouseIndexTemplate houseIndexTemplate) {
 
         DeleteByQueryRequestBuilder builder = DeleteByQueryAction.INSTANCE.newRequestBuilder(esClient)
                 .filter(QueryBuilders.termQuery(HouseIndexKey.HOUSE_ID, houseIndexTemplate.getHouseId()))
