a1cda32cc43641cecceb4a710885233fa84a277b
ccutyang0569@sina.com <ccutyang0569@sina.com>
2018-03-17 17:43:52 +0800

    update

---
+++ src/main/java/com/founder/xunwu/base/HouseSort.java
@@ -0,0 +1,46 @@
+package com.founder.xunwu.base;
+
+import com.google.common.collect.Sets;
+import org.springframework.data.domain.Sort;
+
+import java.util.Set;
+
+/**
+ * @program: xunwu
+ * @description: 排序生辰器
+ * @author: YangMing
+ * @create: 2018-03-17 17:09
+ **/
+public class HouseSort {
+
+    public static final String   DEFAULT_SORT_KEY="lastUpdateTime";
+    public static final String  DISTANCE_TO_SUBWAY_KEY="distanceToSubway";
+
+
+   private static final Set<String> SORT_KEYS= Sets.newHashSet(
+     DEFAULT_SORT_KEY,
+     "createTime",
+      "price",
+      "area",
+      DISTANCE_TO_SUBWAY_KEY
+   );
+
+    public static Sort generateSort(String key,String directionKey){
+        key = getSortKey(key);
+
+        Sort.Direction direction = Sort.Direction.fromStringOrNull(directionKey);
+        if (direction == null) {
+            direction = Sort.Direction.DESC;
+        }
+        return new Sort(direction, key);
+    }
+
+    public static String getSortKey(String key) {
+        if (!SORT_KEYS.contains(key)) {
+            key = DEFAULT_SORT_KEY;
+        }
+        return key;
+    }
+
+
+}
